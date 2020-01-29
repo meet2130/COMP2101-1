ef26d00d7a57a39e325f3421e437b99777c1f150
yug <pyug818@gmail.com>
2020-01-28 22:16:51 -0800

    rolling the dice-lab2

---
+++ bash/diceroller.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+
+# this script rolls a six-sided die
+
+#
+
+
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+
+# rolling the dice and displaying the random number
+
+echo "yug is my name so, there will be three dice which rolles up and give random number"
+
+$(( RANDOM % 6 )), $(( RANDOM % 6 )), $(( RANDOM % 6 ))  rolling
+"
